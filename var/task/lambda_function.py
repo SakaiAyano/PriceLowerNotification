4826e3736e2d37f6b3951363fad3b881a3b8ae99
ayano <a5418ugh@gmail.com>
2022-10-01 00:03:24 +0900

    コメントアウト修正、zip関数の追加

--- var/task/lambda_function.py
+++ var/task/lambda_function.py
@@ -12,6 +12,10 @@ from decimal import Decimal
 dynamo_db = boto3.resource('dynamodb')
 saved_cart_item_table = dynamo_db.Table('saved_cart_item')
 
+amazon_id = os.environ['AMAZON_ID']
+amazon_password = os.environ['AMAZON_PASSWORD']
+slack_url = os.environ['SLACK_URL']
+
 
 # テーブルスキャン
 def table_scan():
@@ -31,7 +35,6 @@ def get_data_price(asin_code):
     return
 
 
-# 項目追加
 def add_record(asin_code, price):
     saved_cart_item_table.put_item(
         Item={
@@ -41,7 +44,6 @@ def add_record(asin_code, price):
     )
 
 
-# 項目削除
 def delete_recodes(deleted_saved_cart_asin_codes):
     with saved_cart_item_table.batch_writer() as batch:
         for deleted_saved_cart_asin_code in deleted_saved_cart_asin_codes:
@@ -51,7 +53,6 @@ def delete_recodes(deleted_saved_cart_asin_codes):
 # Slackへの通知処理
 def price_lower_notification(price_lower_items):
     http = urllib3.PoolManager()
-    slack_url = os.environ['SLACK_URL']
     items_list = ''
     for lower_item_name in price_lower_items:
         items_list += '★' + lower_item_name + '\n'
@@ -65,7 +66,7 @@ def price_lower_notification(price_lower_items):
                 'color': '#D00000',
                 'fields': [
                     {
-                        'title': '以下は20%OFFの商品になります！お買い忘れはございませんか？',
+                        'title': '以下の商品が20%OFFになりました。',
                         'value': items_list
                     }
                 ]
@@ -74,12 +75,7 @@ def price_lower_notification(price_lower_items):
     }
 
     encoded_msg = json.dumps(msg).encode('utf-8')
-    resp = http.request('POST', slack_url, body=encoded_msg)
-    print({
-        'message': 'Hello From Lambda',
-        'status_code': resp.status,
-        'response': resp.data
-    })
+    http.request('POST', slack_url, body=encoded_msg)
 
 
 def lambda_handler(event, context):
@@ -94,22 +90,22 @@ def lambda_handler(event, context):
     # Amazonのログイン画面
     browser.get('https://qr.paps.jp/t6xn7')
     email_elem = browser.find_element_by_id('ap_email')
-    email_elem.send_keys(os.environ['AMAZON_ID'])
+    email_elem.send_keys(amazon_id)
 
     next_button = browser.find_element_by_class_name('a-button-input')
     next_button.click()
     time.sleep(1)
 
     password_elem = browser.find_element_by_id('ap_password')
-    password_elem.send_keys(os.environ['AMAZON_PASSWORD'])
+    password_elem.send_keys(amazon_password)
 
     next_button = browser.find_element_by_id('signInSubmit')
     next_button.click()
     time.sleep(20)
 
-    # Todo:Amazon側ではログイン時にセキュリティメールを送信することがある。登録したメール側から承認する作業を行う必要がある
+    # TODO:Amazon側ではログイン時にセキュリティメールを送信することがある。登録したメール側から承認する作業を行う必要がある
     # ※新しく別ドライブを立ち上げてGmailに自動ログインするのはGoogleの仕様上、現在不可能
-    # Gmailアプリを使用して自動承認機能を追加したい
+    # GmailAPIを使用して自動承認機能を追加したい
 
     cart_button = browser.find_element_by_id('nav-cart')
     cart_button.click()
@@ -125,13 +121,12 @@ def lambda_handler(event, context):
 
     asin_codes = []
     price_lower_items = []
-    i = 0
 
-    for saved_cart_item in saved_cart_items:
+    for saved_cart_item, item_name in zip(saved_cart_items, item_names):
         asin_code = saved_cart_item.get_attribute('data-asin')
-        # Todo:Amazon側では価格情報を小数第一を含めて表示しているが、小数第一を切り捨て対応（小数第一が0以外の商品が損じあスる可能性もあり得る？）
+        # TODO:Amazon側では価格情報を小数第一を含めて表示しているが、小数第一を切り捨て対応（小数第一が0以外の商品が損じあスる可能性もあり得る？）
         price = math.floor(float(saved_cart_item.get_attribute('data-price')))
-        item_name = item_names[i].get_attribute("innerHTML")
+        item_name = item_name.get_attribute("innerHTML")
 
         # 在庫切れ商品や出品者が取り下げた商品については価格情報が0円で取得されているためそういった商品は取り扱わない
         if price != 0:
@@ -144,7 +139,6 @@ def lambda_handler(event, context):
                     price_lower_items.append(item_name)
 
         asin_codes.append(asin_code)
-        i += 1
 
     if len(price_lower_items) != 0:
         price_lower_notification(price_lower_items)
@@ -158,9 +152,6 @@ def lambda_handler(event, context):
     # dynamoDBには存在するがcart内には存在しないasinコードを検索→cart画面から削除された商品
     deleted_saved_cart_asin_codes = set(data_asin_codes) ^ set(asin_codes)
 
-    # if len(deleted_saved_cart_asin_codes) != 0:
-    #     for deleted_saved_cart_asin_code in deleted_saved_cart_asin_codes:
-    #         delete_recode(deleted_saved_cart_asin_code)
     if len(deleted_saved_cart_asin_codes) != 0:
         delete_recodes(deleted_saved_cart_asin_codes)
 
